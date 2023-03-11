8710ca0fc975a803ae1a2e74edcb0d69624b9128
gabi-0 <gmihalcea5@gmail.com>
2023-03-11 14:15:07 +0200

    db load final

--- init.php
+++ init.php
@@ -3,8 +3,8 @@
 
 if(!isset($db)) {
 
-	$__servername = "localhost";
-	$__username = "basic";
+	$__servername = file_get_contents("sv.hid");
+	$__username = file_get_contents("u.hid");
 	$__password = file_get_contents("p.hid");
 	$__database = file_get_contents("f.hid");
 	//$__database = "proces_v.7";
